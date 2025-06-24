2de20fe69c7a150c3b2123ccc433315433bc7a45
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-06-24 16:43:57 +0200

    refactor: extract scala-prolog i/o libraries

---
+++ src/main/scala/it/unibo/prologintegration/Prolog2Scala.scala
@@ -0,0 +1,40 @@
+package it.unibo.prologintegration
+
+import alice.tuprolog.{SolveInfo, Struct, Term, Theory, Var}
+import it.unibo.model.{Cardinals, Diagonals, Direction}
+import it.unibo.prologintegration.Scala2Prolog.Engine
+
+object Prolog2Scala:
+  given Conversion[Term, String] = _.toString
+  given Conversion[Seq[_], Term] = s => Term createTerm s.mkString("[", ",", "]")
+  
+  extension (engine: Engine)
+    def solveWithSuccess(goal: Term): Boolean = engine(goal).map(_.isSuccess).headOption contains true
+    def solveOneAndGetTerm(goal: Term, term: String): Term = engine(goal).headOption.map(extractTerm(_, term)).get
+
+  def extractTerm(solveInfo: SolveInfo, i: Integer): Term =
+    (solveInfo.getSolution.asInstanceOf[Struct] getArg i).getTerm
+
+  def extractTerm(solveInfo: SolveInfo, s: String): Term =
+    solveInfo getTerm s
+
+  def extractListFromTerm(t: Term): LazyList[String] = t match
+    case struct: Struct if struct.getName == "." =>
+      LazyList((struct getArg 0).toString) ++ extractListFromTerm(struct getArg 1)
+    case v: Var => extractListFromTerm(v.getTerm)
+    case _ => LazyList.empty
+
+  extension (solutions: LazyList[SolveInfo])
+    def extractSolutionsOf(target: String): LazyList[String] = solutions map (extractTerm(_, s"$target"))
+
+  def getEnum(s: String): Direction =
+    try
+      Cardinals valueOf s
+    catch
+      case e: IllegalArgumentException =>
+        Diagonals valueOf s
+
+  @main
+  def testProlog2Scala(): Unit =
+    println:
+      getEnum("rightUp")
---
+++ src/main/scala/it/unibo/prologintegration/Scala2Prolog.scala
@@ -0,0 +1,47 @@
+package it.unibo.prologintegration
+
+import alice.tuprolog.{Prolog, SolveInfo, Term, Theory}
+
+import scala.io.Source
+
+object Scala2Prolog:
+  type Engine = Term => LazyList[SolveInfo]
+  given Conversion[String, Theory] = new Theory(_)
+
+  def mkPrologEngine(theory: Theory): Engine =
+    val engine = Prolog()
+    engine setTheory theory
+
+    goal =>
+      new Iterable[SolveInfo] {
+        override def iterator: Iterator[SolveInfo] = new Iterator[SolveInfo]:
+          var solution: Option[SolveInfo] = Some(engine solve goal)
+
+          override def hasNext: Boolean = solution.isDefined &&
+            (solution.get.isSuccess || solution.get.hasOpenAlternatives)
+
+          override def next(): SolveInfo =
+            try solution.get
+            finally solution = if solution.get.hasOpenAlternatives then Some(engine.solveNext()) else None
+      } to LazyList
+
+  def mkPrologEngineFromFile(path: String): Engine =
+    try
+      val source = Source fromFile path
+      val content = source.mkString
+      source.close()
+      mkPrologEngine(content)
+    catch
+      case e: Exception => println(s"Errore nel caricamento del file $path: ${e.getMessage}"); throw e
+  
+  @main def testLoadingTheory(): Unit = 
+    val engine: Engine = mkPrologEngine("""
+      member([H|T],H,T).
+      member([H|T],E,[H|T2]):- member(T,E,T2).
+      permutation([],[]).
+      permutation(L,[H|TP]) :- member(L,H,T), permutation(T,TP).
+    """)
+  
+    val goal = Term createTerm "permutation([1,2,3],L)"
+    engine(goal) foreach println
+    // permutation([1,2,3],[1,2,3]) ... permutation([1,2,3],[3,2,1])
