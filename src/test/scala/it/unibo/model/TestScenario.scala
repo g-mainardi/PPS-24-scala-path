91acf28105704b8c044587cb4babcb1dc7d12dc0
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-06-25 17:41:53 +0200

    test: refactor scenario testing

--- src/test/scala/it/unibo/model/TestScenario.scala
+++ src/test/scala/it/unibo/model/TestScenario.scala
@@ -4,12 +4,17 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class TestScenario extends AnyFlatSpec with Matchers:
-  "A DummyScenario" should "have initial position" in:
-    val scenario = DummyScenario()
-    scenario.initialPosition.x should be (0)
-    scenario.initialPosition.y should be (0)
-
-  "A DummyScenario" should "generate some tiling" in :
-    val scenario = DummyScenario()
+  "A Scenario" should "generate some tiling" in :
+    val scenario: Scenario = DummyScenario()
     scenario.generateScenario()
     scenario.tiles should not be empty  
+
+  "A Scenario" should "have agent in initial position" in :
+    val scenario: Scenario = DummyScenario()
+    scenario.generateScenario()
+    scenario.agent shouldEqual scenario.initialPosition
+
+  "A DummyScenario" should "have initial position (0, 0)" in :
+    val scenario: DummyScenario = DummyScenario()
+    scenario.initialPosition.x should be(0)
+    scenario.initialPosition.y should be(0)
