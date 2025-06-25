ce51fe195dbd07895f66c7e29de6212617297427
MHS-20 <muhamad.salman2001@gmail.com>
2025-06-25 18:13:27 +0200

    feat: added DSL draft

--- src/main/scala/it/unibo/prologintegration/PlannerTemplate.scala
+++ src/main/scala/it/unibo/prologintegration/PlannerTemplate.scala
@@ -1,12 +1,20 @@
 package it.unibo.prologintegration
 
-import alice.tuprolog.Term
+import alice.tuprolog.{Term, Theory}
 import it.unibo.prologintegration.Scala2Prolog.*
 import it.unibo.prologintegration.Prolog2Scala.*
 
+import scala.io.Source
+
 class PlannerTemplate {
-  def runPlanner(path: String, maxMoves: Int): Option[List[String]] =
-    val engine: Engine = mkPrologEngineFromFile(path)
+  def runPlanner(path: String, maxMoves: Int, from: (Int, Int), to: (Int, Int)): Option[List[String]] =
+    
+    val baseTheory: String = Source.fromFile(path).mkString
+    val initFact = s"init(s(${from._1}, ${from._2}))."
+    val goalFact = s"goal(s(${to._1}, ${to._2}))."
+    
+    val fullTheory: Theory = new Theory (baseTheory + "\n" + initFact + "\n" + goalFact)
+    val engine: Engine = mkPrologEngine(fullTheory)
     
     val goal = Term.createTerm("plan(P, " + maxMoves + ")")
     val solutions = engine(goal)
@@ -22,7 +30,7 @@ class PlannerTemplate {
 
 @main def testPlanner(): Unit =
   val planner = PlannerTemplate()
-  val plan = planner.runPlanner("src/main/prolog/basePlanner.pl", 5)
+  val plan = planner.runPlanner("src/main/prolog/basePlanner.pl", 5, (0,0), (2,2))
 
   plan match
     case Some(path) => println(s"Found path: ${path.mkString(" -> ")}")
