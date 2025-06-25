e80892a11da108913474d259c7329d9ca64922cc
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-06-25 18:12:57 +0200

    feat: add '+' method to Position

--- src/main/scala/it/unibo/model/Tiling.scala
+++ src/main/scala/it/unibo/model/Tiling.scala
@@ -1,7 +1,8 @@
 package it.unibo.model
 
 object Tiling:
-  case class Position(x: Int, y: Int, visited: Boolean = false)
+  case class Position(x: Int, y: Int, visited: Boolean = false):
+    def +(vector: Position): Position = Position(x + vector.x, y + vector.y)
   
   trait Tile:
     protected def pos: Position
