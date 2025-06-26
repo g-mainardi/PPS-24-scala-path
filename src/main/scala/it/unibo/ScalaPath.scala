324caf01a509fded64af0a3c6cd6238634bbf35d
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-06-26 15:49:42 +0200

    chore: moved controller init after the app launch and adjusting tab to 2 spaces

--- src/main/scala/it/unibo/ScalaPath.scala
+++ src/main/scala/it/unibo/ScalaPath.scala
@@ -6,13 +6,16 @@ import it.unibo.view.View
 import scala.swing.{Frame, SimpleSwingApplication}
 
 object ScalaPath extends SimpleSwingApplication:
-    val view: View = View(SimulationControllerImpl)
+  val view: View = View(SimulationControllerImpl)
 
-    SimulationControllerImpl attachView view
+  SimulationControllerImpl attachView view
+
+  val cellSize = 20
+  val gridSize = 9
+  val gridOffset = 50
+
+  override def top: Frame = view
+
+  override def main(args: Array[String]): Unit =
+    super.main(args)
     SimulationControllerImpl.initSimulation()
-
-    val cellSize = 20
-    val gridSize = 9
-    val gridOffset = 50
-
-    override def top: Frame = view
