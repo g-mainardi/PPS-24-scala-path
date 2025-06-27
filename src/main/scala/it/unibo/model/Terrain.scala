665d28b188fae20ef281d02a6b6468d06cf1e4e8
Gabribrele <gabrielebasigli@gmail.com>
2025-06-27 11:29:19 +0200

    refactor: terrain noise generation logic

--- src/main/scala/it/unibo/model/Terrain.scala
+++ src/main/scala/it/unibo/model/Terrain.scala
@@ -1,11 +1,13 @@
 package it.unibo.model
+import scala.util.Random
 import Tiling.*
 
+object PerlinNoise:
+  def getNoise(x: Int, y: Int): Double =
+    Random.nextDouble()
+
+
 class Terrain extends Scenario:
-
-
-  private def getNoise(x: Int, y: Int): Double =
-    1.0
 
   private def getTileFromNoise(noise: Double)(position: Position): Tile =
     if noise < 0.2 then
@@ -14,17 +16,17 @@ class Terrain extends Scenario:
       Grass(position)
     else if noise < 0.7 then
       Rock(position)
-    Lava(position)
+    else
+      Lava(position)
 
 
   def initialPosition: Position = Position(0, 0)
 
-  override def generateScenario(): Unit = {
+  override def generateScenario(): Unit =
     tiles =
       (for
         x <- 0 until Scenario.nCols
         y <- 0 until Scenario.nRows
       yield
-        getTileFromNoise(getNoise(x, y))(Position(x, y))).toList
-  }
+        getTileFromNoise(PerlinNoise.getNoise(x, y))(Position(x, y))).toList
 
