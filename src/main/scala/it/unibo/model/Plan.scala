dbbc5feca22e57a2ab1e88e75a20971bad6fdd41
MHS-20 <muhamadhuseyn.salman@studio.unibo.it>
2025-07-02 17:19:30 +0200

    refactor: changed match case in builder

--- src/main/scala/it/unibo/model/Plan.scala
+++ src/main/scala/it/unibo/model/Plan.scala
@@ -3,4 +3,4 @@ package it.unibo.model
 trait Plan
 
 case class SucceededPlan(directions: List[Direction], numMoves: Int) extends Plan
-case class FailedPlan() extends Plan
+case class FailedPlan(errorMessage: String) extends Plan
--- src/main/scala/it/unibo/prologintegration/PlannerBuilder.scala
+++ src/main/scala/it/unibo/prologintegration/PlannerBuilder.scala
@@ -15,6 +15,7 @@ object Conversions:
       Try(Cardinals.valueOf(s.capitalize)).getOrElse(Diagonals.valueOf(s.capitalize))
 
   given Conversion[(Int, Int), Position] = Position(_, _)
+
   given Conversion[Position, (Int, Int)] = p => (p.x, p.y)
 
 class PlannerBuilder:
@@ -41,9 +42,6 @@ class PlannerBuilder:
       case _ => this.maxMoves = 0
     this
 
-  // TO DO
-  def withGridSize(gridSize: (Int, Int)) = ???
-  
   def withTiles(tiles: List[Tile]): PlannerBuilder =
     val tileFacts = tiles.map {
       case p: Passage => s"passable(${p.x}, ${p.y})."
@@ -54,31 +52,31 @@ class PlannerBuilder:
 
   private object InitPos:
     def unapply(o: Option[(Int, Int)]): Option[String] = o map ((ix, iy) => s"init(s($ix, $iy)).")
+
   private object Goal:
     def unapply(o: Option[(Int, Int)]): Option[String] = o map ((ix, iy) => s"goal(s($ix, $iy)).")
 
   def run: Plan = (initPos, goalPos) match
-    case (None, _) | (_, None) =>
-      println("Planner not fully configured (missing init or goal)")
-      FailedPlan()
+    case (None, _) | (_, None) => FailedPlan("Planner not fully configured (missing init or goal)")
     case (InitPos(initFact), Goal(goalFact)) =>
       val fullTheory = new Theory(s"$initFact\n$goalFact\n$theoryStr")
       val engine: Engine = mkPrologEngine(fullTheory)
       val goal = maxMoves match
         case 0 => Term.createTerm(s"plan(P, M)")
         case _ => Term.createTerm(s"plan(P, $maxMoves)")
-      convertToPlan(engine(goal))
-      // val sols: LazyList[SolveInfo] = mkPrologEngine(fullTheory)(Term.createTerm(s"plan(P, M)"))
 
-  private def convertToPlan(solutions: LazyList[SolveInfo]): Plan =
+      checkSolutions(engine(goal))
+
+  private def checkSolutions(solutions: LazyList[SolveInfo]): Plan = solutions match
+    case solveInfo #:: _ if solveInfo.isSuccess => convertToPlan(solveInfo)
+    case _ => FailedPlan("No valid plan found")
+
+  private def convertToPlan(solveInfo: SolveInfo): Plan =
     import Conversions.given
-    solutions.headOption.get match
-      case solveInfo if !solveInfo.isSuccess => FailedPlan()
-      case solveInfo if solveInfo.isSuccess =>
-        val listTerm: Term = extractTerm(solveInfo, "P")
-        val movesTerm: Term = extractTerm(solveInfo, "M")
-        val directions: List[String] = extractListFromTerm(listTerm).toList
-        SucceededPlan(directions.map(s => s: Direction), movesTerm.toString.toInt)
+    val listTerm: Term = extractTerm(solveInfo, "P")
+    val movesTerm: Term = extractTerm(solveInfo, "M")
+    val directions: List[String] = extractListFromTerm(listTerm).toList
+    SucceededPlan(directions.map(s => s: Direction), movesTerm.toString.toInt)
 
 object PlannerBuilder:
   def apply(): PlannerBuilder = new PlannerBuilder()
