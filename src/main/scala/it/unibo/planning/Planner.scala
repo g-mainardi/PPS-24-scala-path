a0b87421fd7968ec3c2dc922205e98048858c065
MHS-20 <muhamadhuseyn.salman@studio.unibo.it>
2025-07-04 16:31:32 +0200

    test: updated test with new builder API

--- src/main/scala/it/unibo/planning/Planner.scala
+++ src/main/scala/it/unibo/planning/Planner.scala
@@ -1,8 +1,9 @@
 package it.unibo.planning
 
+import it.unibo.model.Direction
 import it.unibo.planning.Plan.*
 import it.unibo.model.Tiling.Tile
-import it.unibo.model.Direction.{Cardinals, Diagonals, allDirections}
+import it.unibo.model.Direction.{Cardinals, allDirections}
 import it.unibo.planning.prologplanner.BasePrologPlannerBuilder
 
 // Cardinals.values.toList ++ Diagonals.values.toList
@@ -13,7 +14,7 @@ trait Planner:
 class DummyPlanner extends Planner:
   override def plan: Plan = SucceededPlanWithMoves(List.fill(5)(Cardinals.Down), 5)
 
-class PlannerWithTiles(initPos: (Int, Int), goal: (Int, Int), tiles: List[Tile], maxMoves: Option[Int] = None) extends Planner:
+class PlannerWithTiles(initPos: (Int, Int), goal: (Int, Int), tiles: List[Tile], maxMoves: Option[Int] = None, directions: List[Direction] = allDirections) extends Planner:
   override def plan: Plan =
     println(s"Tiles: $tiles")
     BasePrologPlannerBuilder()
@@ -22,10 +23,10 @@ class PlannerWithTiles(initPos: (Int, Int), goal: (Int, Int), tiles: List[Tile],
       .withInit(initPos)
       .withGoal(goal)
       .withMaxMoves(maxMoves)
-      .withDirections(allDirections)
+      .withDirections(directions)
       .run
 
-class PlannerWithSpecials(initPos: (Int, Int), goal: (Int, Int), tiles: List[Tile], maxMoves: Option[Int] = None) extends Planner:
+class PlannerWithSpecials(initPos: (Int, Int), goal: (Int, Int), tiles: List[Tile], maxMoves: Option[Int] = None, directions: List[Direction] = allDirections) extends Planner:
   override def plan: Plan =
     println(s"Tiles: $tiles")
     BasePrologPlannerBuilder()
--- src/test/scala/it/unibo/prologintegration/TestPlanner.scala
+++ src/test/scala/it/unibo/prologintegration/TestPlanner.scala
@@ -1,20 +1,36 @@
 package it.unibo.prologintegration
 
-import it.unibo.model.Tiling.{Floor, Position, Tile, Wall}
+import it.unibo.model.Tiling.{Floor, Position, Teleport, Tile, Wall}
+
+import scala.util.Random
 
 trait TestPlanner {
+
+  val gridrows: Int = 3
+  val gridcols: Int = 3
+
   val passableTiles: List[Tile] = (
     for {
-      x <- 0 to 2
-      y <- 0 to 2
+      x <- 0 to gridrows
+      y <- 0 to gridcols
     } yield Floor(Position(x, y))
     ).toList
 
   val blockingTiles: List[Tile] = (
     for {
-      x <- 0 to 2
-      y <- 0 to 2
+      x <- 0 to gridrows
+      y <- 0 to gridcols
     } yield Wall(Position(x, y))
     ).toList
 
-}
+  val specialTiles: List[Tile] = (
+    for {
+      x <- 0 to gridrows
+      y <- 0 to gridcols
+    } yield {
+      val pos = Position(x, y)
+      if (x == 1 && y == 1) Teleport(pos)
+      if (x == 2 && y == 2) Teleport(pos)
+      else Floor(pos)
+    }).toList
+    }
