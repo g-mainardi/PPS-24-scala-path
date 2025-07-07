a1b378980c630829e08d42d53b89cd34f4e54889
MHS-20 <muhamad.salman2001@gmail.com>
2025-07-07 16:03:22 +0200

    feat: finalized moves generations and removed directions generation

--- src/main/scala/it/unibo/planning/prologplanner/BasePrologPlannerBuilder.scala
+++ src/main/scala/it/unibo/planning/prologplanner/BasePrologPlannerBuilder.scala
@@ -38,41 +38,10 @@ class BasePrologPlannerBuilder extends PrologBuilder:
       case o: Obstacle => s"blocked(${o.x}, ${o.y})."
     }.mkString("\n"))
 
-  //  private object Directions:
-  //    def unapply(o: Option[List[Direction]]): Option[String] = o map (direction => direction.map {
-  //      case c: Cardinals => s"cardinals(${c.toString.toLowerCase})."
-  //      case d: Diagonals => s"diagonals(${d.toString.toLowerCase})."
-  //    }.mkString("\n"))
-
   private object Directions:
     def unapply(o: Option[List[Direction]]): Option[String] = o map { directions =>
-      val hasCardinals = directions.exists(_.isInstanceOf[Cardinals])
-      val hasDiagonals = directions.exists(_.isInstanceOf[Diagonals])
-      val facts = directions.map {
-        case c: Cardinals => s"cardinals(${c.toString.toLowerCase})."
-        case d: Diagonals => s"diagonals(${d.toString.head.toLower + d.toString.tail})."
-        case s: Special => s"special(${s.toString.toLowerCase})."
-      }
-      val header = Seq(
-        if (hasCardinals) Some("directions(D):- cardinals(D).") else None,
-        if (hasDiagonals) Some("directions(D):- diagonals(D).") else None
-      ).flatten
-      (facts ++ header).mkString("\n") + generateMoveRules(directions)
+      generateMoveRules(directions)
     }
-
-    // extension Method per mappare ai blocchi moves salvati su file?
-    // oppure generazione dinamica a runtime, così non serve sapere a priori le dirs?
-    // Tanto le dirs sono più statiche rispetto ai tiles
-    extension(direction: Direction)
-        def moveFact: String = direction match
-          case Cardinals.Down =>   Source.fromFile("src/main/prolog/moves/down.pl").mkString
-          case Cardinals.Up =>     Source.fromFile("src/main/prolog/moves/up.pl").mkString
-          case Cardinals.Left =>   Source.fromFile("src/main/prolog/moves/left.pl").mkString
-          case Cardinals.Right =>  Source.fromFile("src/main/prolog/moves/right.pl").mkString
-          case Diagonals.LeftDown => Source.fromFile("src/main/prolog/moves/downLeft.pl").mkString
-          case Diagonals.RightDown => Source.fromFile("src/main/prolog/moves/downRight.pl").mkString
-          case Diagonals.LeftUp => Source.fromFile("src/main/prolog/moves/upLeft.pl").mkString
-          case Diagonals.RightUp => Source.fromFile("src/main/prolog/moves/upRight.pl").mkString
 
   object IncompletePlannerConfig:
     def unapply(config: (Option[(Int, Int)], Option[(Int, Int)], Option[String], Option[List[Tile]], Option[List[Direction]])): Option[String] =
@@ -81,16 +50,6 @@ class BasePrologPlannerBuilder extends PrologBuilder:
       labels.zip(options).collectFirst {
         case (label, None) => s"missing $label"
       }
-
-  //  object IncompletePlannerConfig:
-  //    def unapply(config: (Option[(Int, Int)], Option[(Int, Int)], Option[String], Option[List[Tile]], Option[List[Direction]])): Option[String] =
-  //      config match
-  //        case (None, _, _, _, _) => Some("missing init position")
-  //        case (_, None, _, _, _) => Some("missing goal")
-  //        case (_, _, None, _, _) => Some("missing theory")
-  //        case (_, _, _, None, _) => Some("missing environmental tiles")
-  //        case (_, _, _, _, None) => Some("missing possible directions")
-  //        case _ => None
 
   override def run: Plan = (initPos, goalPos, theoryPath, environmentTiles, directions) match
     case IncompletePlannerConfig(reason) => FailedPlan(s"Planner not fully configured, $reason")
---
+++ src/main/scala/it/unibo/planning/prologplanner/TempStuff.scala
@@ -0,0 +1,41 @@
+package it.unibo.planning.prologplanner
+
+class TempStuff {
+
+  // extension Method per mappare ai blocchi moves salvati su file?
+  // oppure generazione dinamica a runtime, così non serve sapere a priori le dirs?
+  // Tanto le dirs sono più statiche rispetto ai tiles
+//  extension (direction: Direction)
+//    def moveFact: String = direction match
+//      case Cardinals.Down => Source.fromFile("src/main/prolog/moves/down.pl").mkString
+//      case Cardinals.Up => Source.fromFile("src/main/prolog/moves/up.pl").mkString
+//      case Cardinals.Left => Source.fromFile("src/main/prolog/moves/left.pl").mkString
+//      case Cardinals.Right => Source.fromFile("src/main/prolog/moves/right.pl").mkString
+//      case Diagonals.LeftDown => Source.fromFile("src/main/prolog/moves/downLeft.pl").mkString
+//      case Diagonals.RightDown => Source.fromFile("src/main/prolog/moves/downRight.pl").mkString
+//      case Diagonals.LeftUp => Source.fromFile("src/main/prolog/moves/upLeft.pl").mkString
+//      case Diagonals.RightUp => Source.fromFile("src/main/prolog/moves/upRight.pl").mkString
+
+}
+
+//  private object Directions:
+//    def unapply(o: Option[List[Direction]]): Option[String] = o map (direction => direction.map {
+//      case c: Cardinals => s"cardinals(${c.toString.toLowerCase})."
+//      case d: Diagonals => s"diagonals(${d.toString.toLowerCase})."
+//    }.mkString("\n"))
+
+//private object Directions:
+//  def unapply(o: Option[List[Direction]]): Option[String] = o map { directions =>
+//    val hasCardinals = directions.exists(_.isInstanceOf[Cardinals])
+//    val hasDiagonals = directions.exists(_.isInstanceOf[Diagonals])
+//    val facts = directions.map {
+//      case c: Cardinals => s"cardinals(${c.toString.toLowerCase})."
+//      case d: Diagonals => s"diagonals(${d.toString.head.toLower + d.toString.tail})."
+//      case s: Special => s"special(${s.toString.toLowerCase})."
+//    }
+//    val header = Seq(
+//      if (hasCardinals) Some("directions(D):- cardinals(D).") else None,
+//      if (hasDiagonals) Some("directions(D):- diagonals(D).") else None
+//    ).flatten
+//    (facts ++ header).mkString("\n") + generateMoveRules(directions)
+//  }
