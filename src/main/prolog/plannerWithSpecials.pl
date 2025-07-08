0318a3770fac75aed1e232b96561c8fbd6d2519d
MHS-20 <muhamad.salman2001@gmail.com>
2025-07-08 16:16:19 +0200

    chore: removed double member in planner

--- src/main/prolog/plannerWithSpecials.pl
+++ src/main/prolog/plannerWithSpecials.pl
@@ -4,6 +4,8 @@ plan(Path, Moves) :-
     goal(Goal),
     planner(Init, Goal, [Init], Path, Moves).
 
+
+
 % Base case: at goal, no moves needed
 planner(State, State, _, [], 0).
 
@@ -11,10 +13,11 @@ checkSpecial(TempState, NewState) :- special(TempState, NewState), !.
 checkSpecial(TempState, TempState).
 
 % Recursive case: explore possible directions
+% planner(+CurrentState, +GoalState, +VisitedStates, -Path, -MoveCount)
 planner(State, Goal, Visited, [Dir|Rest], NewMoves) :-
     %% directions(Dir),
     move(State, Dir, TempState),
-    \+ member(TempState, Visited),
+    %% \+ member(TempState, Visited),
     checkSpecial(TempState, NewState),
     \+ member(NewState, Visited),
     planner(NewState, Goal, [NewState|Visited], Rest, Moves),
