26e7de0a4c2763fda2b08b089fa8a11694bd7047
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-07-10 11:35:06 +0200

    feat: optimized bfs prolog search

--- src/main/prolog/bfs.pl
+++ src/main/prolog/bfs.pl
@@ -13,22 +13,29 @@ bfs([Steps | _], Max, Dirs, Path) :-    % case: goal reached
   (var(Max) -> length(Dirs, Max); true).
 
 bfs([Steps|Others], Max, Dirs, Path) :- % case: recursion on queue
-	Steps = [c(P, _) | T],
+	Steps = [c(P, _) | _],
 	% if Max is specified then I have to keep track of the lenght
 	% if not, I will calculate the lenght only at the end
   (number(Max) -> length(Steps, L), L =< Max + 1 ; true),  % +1 because first move is 'none'
-  find_new_paths(Steps, NewPaths),
+  find_new_paths(Steps, Others, NewPaths),
   append(Others, NewPaths, QueueNext),
   bfs(QueueNext, Max, Dirs, Path).
 
-% find_new_paths(+Steps, -NewPaths):-
-find_new_paths(Steps, NewPaths):-
+% find_new_paths(+Steps, +Others, -NewPaths):-
+find_new_paths(Steps, Others, NewPaths):-
 	Steps = [c(P, _) | T],
   findall(
   	[c(Next, Dir)| Steps],
-    (move(P, Dir, Next), \+ member(c(Next, _), T)),
+  	% search a move to a position not in this trace or in the head of the others
+  	search_next(P, T, Others, Next, Dir),
     NewPaths
   ).
+
+% search_next(+Position, +Visited, +OtherPaths, -NextPosition, -ChoosenDirection)
+search_next(Src, Trace, Others, Next, Dir):-
+  move(Src, Dir, Next),
+  \+ member(c(Next, _), Trace),
+  \+ member([c(Next, _)|_], Others).
 
 % extract(+Queue, -Path, -Directions)
 extract([], [], []).
