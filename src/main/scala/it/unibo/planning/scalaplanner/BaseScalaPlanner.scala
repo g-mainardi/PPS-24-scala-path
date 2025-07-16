c4fb5dad620415251f161e7f5b20c8e25bc758e0
Gabriele Basigli <84378604+Basigli@users.noreply.github.com>
2025-07-16 12:24:14 +0200

    refactor: now ScalaPlanner returns a SucceededPlanWithMoves

--- src/main/scala/it/unibo/planning/scalaplanner/BaseScalaPlanner.scala
+++ src/main/scala/it/unibo/planning/scalaplanner/BaseScalaPlanner.scala
@@ -2,11 +2,12 @@ package it.unibo.planning.scalaplanner
 
 import it.unibo.model.Direction
 import it.unibo.planning.Plan
-import it.unibo.planning.Plan.{FailedPlan, SucceededPlan}
+import it.unibo.planning.Plan.{FailedPlan, SucceededPlan, SucceededPlanWithMoves}
 
 trait BaseScalaPlanner:
   def checkSolution(directions: Option[List[Direction]]): Plan =
     if directions.nonEmpty then
-      SucceededPlan(directions.get)
+      // SucceededPlan(directions.get)
+      SucceededPlanWithMoves(directions.get, directions.get.length)
     else
       FailedPlan("No valid plan found")
