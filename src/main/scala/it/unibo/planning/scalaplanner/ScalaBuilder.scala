3a8f785bae6ce142af87d111a9d6faf875eb26b6
MHS-20 <muhamadhuseyn.salman@studio.unibo.it>
2025-07-16 13:11:41 +0200

    fix: to agent for scala builder

--- src/main/scala/it/unibo/planning/scalaplanner/ScalaBuilder.scala
+++ src/main/scala/it/unibo/planning/scalaplanner/ScalaBuilder.scala
@@ -6,11 +6,7 @@ import it.unibo.planning.{Configuration, Plan, Planner, ScalaPlanner}
 import it.unibo.planning.prologplanner.Conversions.given_Conversion_Int_Int_Position
 
 class ScalaBuilder(using configuration: Configuration):
-  def build: Agent =
-    new Agent(
-      configuration.initPos,
-      () => ScalaPlanner(configuration.initPos, configuration.goalPos, configuration.environmentTiles.tiles, configuration.directions, configuration.algorithm.get).plan,
-      configuration.environmentTiles.checkSpecial,
-    )
-    
+  def build: Planner = 
+    ScalaPlanner(configuration.initPos, configuration.goalPos, configuration.environmentTiles.tiles, configuration.directions, configuration.algorithm.get)
+  
     
