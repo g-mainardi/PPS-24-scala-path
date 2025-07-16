dcaa5da2a6b03f1fb0026310c2f01943ac09f6e2
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-07-16 18:10:09 +0200

    test: dummy scenario comeback

--- src/test/scala/it/unibo/model/TestScenario.scala
+++ src/test/scala/it/unibo/model/TestScenario.scala
@@ -1,12 +1,19 @@
 package it.unibo.model
 
-import it.unibo.model.Tiling.{Position, Teleport, Tile}
+import it.unibo.model.Tiling.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import scala.util.Random
 
 class TestScenario extends AnyFlatSpec with Matchers:
+  class DummyScenario(nRows: Int, nCols: Int) extends Scenario(nRows, nCols):
+    override def generate(): Unit =
+      _tiles = for
+        (tileType, ind) <- List(Floor(_), Grass(_), Trap(_), Water(_), Lava(_), Rock(_)).zipWithIndex
+        pos: Position = Position(ind, ind)
+      yield
+        tileType(pos)
   "A Scenario" should "generate some tiling" in :
     val scenario: Scenario = DummyScenario(3, 3)
     scenario.generate()
