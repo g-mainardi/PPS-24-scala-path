1b235385bae75b5b1b7d176c4ee392801225350e
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-07-17 23:15:43 +0200

    fix: reset start and stop button on reset

--- src/main/scala/it/unibo/controller/ViewManager.scala
+++ src/main/scala/it/unibo/controller/ViewManager.scala
@@ -11,11 +11,11 @@ trait ViewManager:
   
   final private def applyToView(viewAction: View => Unit): Unit = onEDT:
     _view foreach viewAction
-    
+
   protected object View:
     def update(): Unit = applyToView: v =>
       v.repaint()
-    
+
     def disableControls(): Unit = applyToView: v =>
       v.disableStepButton()
       v.disableResetButton()
@@ -23,35 +23,36 @@ trait ViewManager:
 
     def pause(): Unit = applyToView: v =>
       v.enableStepButton()
-    
+
     def firstStep(): Unit = applyToView: v =>
       v.enableResetButton()
-  
+
     def resume(): Unit = applyToView: v =>
       v.enableStartStopButton()
       v.enableResetButton()
       v.disableStepButton()
-    
+
     def reset(): Unit = applyToView: v =>
       v.disableResetButton()
+      v.resetStartStopButton()
       v.enableStartStopButton()
       v.enableStepButton()
-    
+
     def over(): Unit = applyToView: v =>
       v.disableStepButton()
       v.disableStartStopButton()
       v.showInfoMessage("Plan terminated! You can restart it or try a new one.", "End of plan")
-        
+
     def noPlanFound(msg: String): Unit = applyToView: v =>
       v.closeLoadingDialog()
       v.showErrorMessage(msg, "No plan found")
-        
+
     def planFound(msg: String): Unit = applyToView: v =>
       v.closeLoadingDialog()
       v.enableStepButton()
       v.enableStartStopButton()
       v.showInfoMessage(msg, "Plan found")
-      
+
     def search(): Unit = applyToView: v =>
       v showLoadingDialog "Searching a plan..."
 
