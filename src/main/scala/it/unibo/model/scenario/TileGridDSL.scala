b4f81f53fffeed7a258efcfeb942c715bf9507fc
Gabriele Basigli <84378604+Basigli@users.noreply.github.com>
2025-07-17 22:56:14 +0200

    refactor: improved DSL syntax and usage

--- src/main/scala/it/unibo/model/scenario/TileGridDSL.scala
+++ src/main/scala/it/unibo/model/scenario/TileGridDSL.scala
@@ -1,11 +1,11 @@
 package it.unibo.model.scenario
 
+import scala.language.postfixOps
 import it.unibo.model.fundamentals.Tiling.{Floor, Grass, Wall}
 import it.unibo.model.fundamentals.{Position, Tile}
+import scala.language.implicitConversions
 
-import scala.language.postfixOps
-
-
+// --- DSL symbols ---
 object TileSymbol:
   sealed trait Symbol
   case object F extends Symbol
@@ -17,43 +17,60 @@ class GridBuilder:
   private var currentRow: List[Symbol] = List.empty
   private var rows: List[List[Symbol]] = List.empty
 
-  def |(sym: Symbol): GridBuilder =
+  def add(sym: Symbol): Unit =
     currentRow = currentRow :+ sym
-    this
 
-  def || : GridBuilder =
+  def newRow(): Unit =
     if rows.nonEmpty && currentRow.length != rows.head.length then
-      throw new IllegalArgumentException(s"Rows must be of the same length ${rows.head.length}, found ${currentRow.length}")
+      throw new IllegalArgumentException(s"Rows must be of the same length: expected ${rows.head.length}, found ${currentRow.length}")
     rows = rows :+ currentRow
     currentRow = List.empty
-    this
 
   def build(): List[Tile] =
     val mapping: Map[Symbol, Position => Tile] = Map(
-      F -> (Floor.apply),
-      G -> (Grass.apply),
-      W -> (Wall.apply)
+      F -> Floor.apply,
+      G -> Grass.apply,
+      W -> Wall.apply
     )
-    rows.zipWithIndex.toList.flatMap { case (line, y) =>
+    rows.zipWithIndex.flatMap { case (line, y) =>
       line.zipWithIndex.map { case (sym, x) =>
-        mapping.getOrElse(sym, throw new IllegalArgumentException(s"Non existing symbol: '$sym'"))
-          .apply(Position(x, y))
+        mapping.getOrElse(sym, throw new IllegalArgumentException(s"Unknown symbol: $sym"))(Position(x, y))
       }
     }
 
+
 object GridDSL:
-  def start: GridBuilder = new GridBuilder
+  def grid(body: GridBuilder ?=> Unit): List[Tile] =
+    given builder: GridBuilder = new GridBuilder()
+    body
+    builder.build()
 
+  extension (sym: Symbol)(using builder: GridBuilder)
+    infix def |(next: Symbol) : GridBuilder =
+      builder.add(sym)
+      builder.add(next)
+      builder
 
+    def || : GridBuilder =
+      builder.newRow()
+      builder
+
+  extension (b: GridBuilder)
+    infix def |(next: Symbol): GridBuilder =
+      b.add(next)
+      b
+
+    def || : GridBuilder =
+      b.newRow()
+      b
 
 import GridDSL.*
 import TileSymbol.*
 
 @main def testDSL(): Unit =
-  val tiles = start
-    .|(F).|(F).|(F).||
-    .|(G).|(G).|(G).||
-    .|(W).|(W).|(W).||
-    .build()
+  val tiles = grid:
+    (F | F | F). ||
+    (G | G | G). ||
+    (W | W | W). ||
 
   tiles.foreach(t => println(s"${t.getClass.getSimpleName} at (${t.x}, ${t.y})"))
