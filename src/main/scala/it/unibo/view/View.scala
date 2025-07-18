599faf8de31d2ee1469a30df304565049b2dcbeb
Gabriele Basigli <84378604+Basigli@users.noreply.github.com>
2025-07-18 12:30:42 +0200

    refactor: view panels and components into separate files

--- src/main/scala/it/unibo/view/View.scala
+++ src/main/scala/it/unibo/view/View.scala
@@ -21,104 +21,29 @@ import java.awt.event.MouseAdapter
 
 class View(controller: DisplayableController, gridOffset: Int, cellSize: Int) extends MainFrame:
   import ViewUtilities.*
+  import ViewPanels.*
+  import ViewComponents.*
+
   title = "Scala Path"
   preferredSize = new Dimension(800, 600)
-  private val algorithmDropdown = new ComboBoxWithPlaceholder("Select algorithm", controller.algorithmsNames, Simulation set UICommand.ChangeAlgorithm(_)){enabled = false}
-  private val scenarioDropdown = new ComboBoxWithPlaceholder("Select scenario...", controller.scenariosNames, Simulation set UICommand.ChangeScenario(_))
 
-  private class DirectionGrid() extends GridPanel(3, 3):
-    private val directions: List[Option[Direction]] = List(
-      Some(LeftUp), Some(Up), Some(RightUp),
-      Some(Left), None, Some(Right),
-      Some(LeftDown), Some(Down), Some(RightDown)
-    )
-    private var selectedDirections: Seq[Direction] = Direction.allDirections
-    private val buttonSize = new Dimension(40, 40)
-    contents ++= directions.map { dirOpt =>
-      val onDeselection: () => Unit = dirOpt.map { d => () =>
-        selectedDirections = selectedDirections :+ d
-        Simulation set UICommand.DirectionsChoice(selectedDirections)
-        println("called onDeselection, selectedDirections: " + selectedDirections)
-      }.getOrElse(() => {})
-
-      val onSelection: () => Unit = dirOpt.map { d => () =>
-        selectedDirections = selectedDirections.filterNot(_ == d)
-        Simulation set UICommand.DirectionsChoice(selectedDirections)
-        println("called onSelection, selectedDirections: " + selectedDirections)
-      }.getOrElse(() => {})
-
-      val btn = new SelectionButton(
-        onState1 = onSelection,
-        onState2 = onDeselection
-      ) {
-        preferredSize = buttonSize
-        minimumSize = buttonSize
-        maximumSize = buttonSize
-        enabled = dirOpt.isDefined
-        if enabled then
-          icon = getArrowIconFromDirection(dirOpt.get)
-        selected = false
-      }
-      btn
-    }
-
-  private val refreshScenarioButton = new Button(){
-    icon = scaledIcon("/icons/refreshIcon.png", 14, 14)
-    borderPainted = false
-    contentAreaFilled = false
-    focusPainted = false
-    enabled = false
-  }
-  private val resetButton = new DefaultDisabledButton("Reset")
-  private val stepButton = new DefaultDisabledButton("Step")
-  private val remainingSteps = new Label()
-  private val colsInput = new IntegerTextField() {
-    columns = 2
-    text = controller.scenario.nCols.toString
-  }
-  private val rowsInput = new IntegerTextField() {
-    columns = 2
-    text = controller.scenario.nRows.toString
-  }
   private var loadingDialog: Option[Dialog] = None
 
-  private val directionGrid = new FlowPanel {
-    contents += new DirectionGrid()
-    preferredSize = new Dimension(40*3, 40*3)
-    maximumSize = preferredSize
-    minimumSize = preferredSize
-  }
-  private val startStopButton = new TwoStateButton(
-    "Start",
-    "Stop",
-    Simulation set ExecutionState.Running,
-    Simulation set ExecutionState.Paused(fromUser = true)
-  ){enabled = false}
 
-  private val speedSlider = new Slider {
-    min = 1
-    max = 31
-    value = 10
-    majorTickSpacing = 10
-    minorTickSpacing = 1
-    paintTicks = true
-    paintLabels = true
-    orientation = Orientation.Horizontal
-    preferredSize = new Dimension(200, 40)
-    labels = (min to max by majorTickSpacing).map(i => i -> new Label(f"${i / 10.0}%.1fx")).toMap
-    tooltip = "Animation speed (0.1x - 3.1x)"
-  }
+  private val controlPanel = ControlPanel()
+  private val scenarioSettingsPanel = ScenarioSettingsPanel(controller)
 
-  def enableStepButton(): Unit = stepButton.enabled = true
-  def disableStepButton(): Unit = stepButton.enabled = false
-  def enableResetButton(): Unit = resetButton.enabled = true
-  def disableResetButton(): Unit = resetButton.enabled = false
-  def enableStartStopButton(): Unit = startStopButton.enabled = true
-  def disableStartStopButton(): Unit = startStopButton.enabled = false
-  def enableRefreshScenarioButton(): Unit = refreshScenarioButton.enabled = true
-  def enableAlgorithmDropdown(): Unit = algorithmDropdown.enabled = true
-  def resetAlgorithmDropdown(): Unit = algorithmDropdown.reset()
-  def resetStartStopButton(): Unit = startStopButton.reset()
+  def enableStepButton(): Unit = controlPanel.stepButton.enabled = true
+  def disableStepButton(): Unit = controlPanel.stepButton.enabled = false
+  def enableResetButton(): Unit = controlPanel.resetButton.enabled = true
+  def disableResetButton(): Unit = controlPanel.resetButton.enabled = false
+  def resetStartStopButton(): Unit = controlPanel.startStopButton.reset()
+  def enableStartStopButton(): Unit = controlPanel.startStopButton.enabled = true
+  def disableStartStopButton(): Unit = controlPanel.startStopButton.enabled = false
+
+  def enableRefreshScenarioButton(): Unit = scenarioSettingsPanel.refreshScenarioButton.enabled = true
+  def enableAlgorithmDropdown(): Unit = scenarioSettingsPanel.algorithmDropdown.enabled = true
+  def resetAlgorithmDropdown(): Unit = scenarioSettingsPanel.algorithmDropdown.reset()
 
   def showInfoMessage(message: String, title: String): Unit =
     showPopupMessage(message, title, Dialog.Message.Info)
@@ -133,13 +58,13 @@ class View(controller: DisplayableController, gridOffset: Int, cellSize: Int) ex
     case Some(dialog) => ViewUtilities closeLoadingDialog dialog
     case None         => throw IllegalStateException("No loading dialog to close")
 
-  private val moveStartRadio = new RadioButton("Start")
-  private val moveGoalRadio = new RadioButton("Goal") { selected = true }
-
-  private val moveGroup = new ButtonGroup(moveStartRadio, moveGoalRadio)
-
-  private val movePanel = new FlowPanel(moveStartRadio, moveGoalRadio)
-
+  private val directionGrid = new FlowPanel {
+    contents += new DirectionGrid()
+    preferredSize = new Dimension(40 * 3, 40 * 3)
+    maximumSize = preferredSize
+    minimumSize = preferredSize
+  }
+  
   private val gridPanel: Panel = new Panel:
     preferredSize = new Dimension(200, 100)
     listenTo(mouse.clicks)
@@ -148,9 +73,9 @@ class View(controller: DisplayableController, gridOffset: Int, cellSize: Int) ex
         val x = (e.point.x - gridOffset) / cellSize
         val y = (e.point.y - gridOffset) / cellSize
         if x >= 0 && y >= 0 && x < controller.scenario.nCols && y < controller.scenario.nRows then
-          if moveGoalRadio.selected then {
+          if scenarioSettingsPanel.moveGoalRadio.selected then {
             Simulation set UICommand.SetPosition(Simulation.SettablePosition.Goal(x, y))
-          } else if moveStartRadio.selected then
+          } else if scenarioSettingsPanel.moveStartRadio.selected then
             Simulation set UICommand.SetPosition(Simulation.SettablePosition.Init(x, y))
     }
     override def paintComponent(g: Graphics2D): Unit =
@@ -163,7 +88,7 @@ class View(controller: DisplayableController, gridOffset: Int, cellSize: Int) ex
       controller.agent foreach: agent =>
         drawCircle(agent.x, agent.y, Color.YELLOW)
         drawPath(agent.path)
-        remainingSteps.text = agent.remainingSteps.toString
+        controlPanel.remainingSteps.text = agent.remainingSteps.toString
 
     private def drawPath(positions: Seq[(Position, Direction)])(using g: Graphics2D): Unit =
       positions.foreach((p, d) =>
@@ -196,40 +121,9 @@ class View(controller: DisplayableController, gridOffset: Int, cellSize: Int) ex
         g setColor Color(147, 153, 149)
         g draw rect
 
-
-  // private object ControlPanel extends FlowPanel(startButton, stepButton, resetButton, startStopButton, scenarioDropdown, algorithmDropdown, refreshScenarioButton)
-  private object ControlPanel extends FlowPanel(startStopButton, stepButton,
-    new Label("Remaining steps: "), remainingSteps, resetButton,
-    new Label("Animation speed: "), speedSlider)
-  private object ScenarioSettingsPanel extends FlowPanel(new Label("Dimensions: "), colsInput, new Label("x"), rowsInput,
-    new Label("Search with: "), scenarioDropdown, refreshScenarioButton,
-    new Label("Change: "), movePanel, algorithmDropdown)
-
   contents = new BorderPanel:
     import BorderPanel.Position
     layout(gridPanel) = Position.Center
-    layout(ControlPanel) = Position.South
-    layout(ScenarioSettingsPanel) = Position.North
+    layout(controlPanel) = Position.South
+    layout(scenarioSettingsPanel) = Position.North
     layout(directionGrid) = Position.West
-
-  listenTo(stepButton, resetButton, scenarioDropdown.selection, algorithmDropdown.selection, refreshScenarioButton, speedSlider, colsInput, rowsInput)
-  reactions += {
-    case ButtonClicked(`stepButton`) => Simulation set ExecutionState.Step
-    case ButtonClicked(`resetButton`) => Simulation set ExecutionState.Empty
-    case ButtonClicked(`refreshScenarioButton`) => Simulation set UICommand.ChangeScenario(scenarioDropdown.selection.index)
-    case ValueChanged(`speedSlider`) =>
-      val speed = speedSlider.value / 10.0
-      Simulation set UICommand.SetAnimationSpeed(1 / speed)
-    case event.EditDone(`colsInput`) => onDimensionChange()
-    case event.EditDone(`rowsInput`) => onDimensionChange()
-  }
-
-  private def onDimensionChange(): Unit =
-    val maybeRows = rowsInput.text.toIntOption
-    val maybeCols = colsInput.text.toIntOption
-
-    (maybeRows, maybeCols) match
-      case (Some(rows), Some(cols)) if rows > 0 && cols > 0 =>
-        Simulation set UICommand.SetScenarioSize(rows, cols)
-        println(s"passing ($rows $cols)")
-      case _ => None
---
+++ src/main/scala/it/unibo/view/ViewPanels.scala
@@ -0,0 +1,112 @@
+package it.unibo.view
+
+import it.unibo.controller.{DisplayableController, Simulation}
+import it.unibo.controller.Simulation.{ExecutionState, UICommand}
+import it.unibo.view.ViewUtilities.{ComboBoxWithPlaceholder, DefaultDisabledButton, IntegerTextField, TwoStateButton, scaledIcon}
+
+import scala.swing.event.{ButtonClicked, EditDone, ValueChanged}
+import scala.swing.{Button, ButtonGroup, Component, Dimension, FlowPanel, Label, Orientation, RadioButton, Slider}
+
+object ViewPanels:
+  object ControlPanel:
+    def apply(): ControlPanel = new ControlPanel()
+
+  class ControlPanel() extends FlowPanel:
+    val startStopButton: TwoStateButton = new TwoStateButton(
+      "Start",
+      "Stop",
+      Simulation set ExecutionState.Running,
+      Simulation set ExecutionState.Paused(fromUser = true)
+    ) {
+      enabled = false
+    }
+    val stepButton = new DefaultDisabledButton("Step")
+    val remainingSteps = new Label()
+    val resetButton = new DefaultDisabledButton("Reset")
+
+    val speedSlider: Slider = new Slider {
+      min = 1
+      max = 31
+      value = 10
+      majorTickSpacing = 10
+      minorTickSpacing = 1
+      paintTicks = true
+      paintLabels = true
+      orientation = Orientation.Horizontal
+      preferredSize = new Dimension(200, 40)
+      labels = (min to max by majorTickSpacing).map(i => i -> new Label(f"${i / 10.0}%.1fx")).toMap
+      tooltip = "Animation speed (0.1x - 3.1x)"
+    }
+
+    contents ++= Seq(
+      startStopButton, stepButton,
+      new Label("Remaining steps: "), remainingSteps,
+      resetButton,
+      new Label("Animation speed: "), speedSlider
+    )
+    listenTo(stepButton, resetButton, speedSlider)
+    reactions += {
+      case ButtonClicked(`stepButton`) => Simulation set ExecutionState.Step
+      case ButtonClicked(`resetButton`) => Simulation set ExecutionState.Empty
+      case ValueChanged(`speedSlider`) =>
+        val speed = speedSlider.value / 10.0
+        Simulation set UICommand.SetAnimationSpeed(1 / speed)
+    }
+
+
+  object ScenarioSettingsPanel:
+    def apply(controller: DisplayableController): ScenarioSettingsPanel =
+    new ScenarioSettingsPanel(controller)
+
+  class ScenarioSettingsPanel(controller: DisplayableController) extends FlowPanel:
+    val colsInput: IntegerTextField = new IntegerTextField() {
+      columns = 2
+      text = controller.scenario.nCols.toString
+    }
+    val rowsInput: IntegerTextField = new IntegerTextField() {
+      columns = 2
+      text = controller.scenario.nRows.toString
+    }
+    val algorithmDropdown: ComboBoxWithPlaceholder[String] = new ComboBoxWithPlaceholder(
+      "Select algorithm",
+      controller.algorithmsNames,
+      Simulation set UICommand.ChangeAlgorithm(_)
+    ) { enabled = false }
+    val scenarioDropdown = new ComboBoxWithPlaceholder(
+      "Select scenario...",
+      controller.scenariosNames,
+      Simulation set UICommand.ChangeScenario(_)
+    )
+    val refreshScenarioButton: Button = new Button() {
+      icon = scaledIcon("/icons/refreshIcon.png", 14, 14)
+      borderPainted = false
+      contentAreaFilled = false
+      focusPainted = false
+      enabled = false
+    }
+    val moveStartRadio = new RadioButton("Start")
+    val moveGoalRadio = new RadioButton("Goal") { selected = true }
+    val moveGroup = new ButtonGroup(moveStartRadio, moveGoalRadio)
+    val movePanel = new FlowPanel(moveStartRadio, moveGoalRadio)
+
+    contents ++= Seq(
+      new Label("Dimensions: "), colsInput, new Label("x"), rowsInput,
+      new Label("Search with: "), scenarioDropdown, refreshScenarioButton,
+      new Label("Change: "), movePanel, algorithmDropdown
+    )
+    listenTo(refreshScenarioButton, colsInput, rowsInput)
+    reactions += {
+      case ButtonClicked(`refreshScenarioButton`) => Simulation set UICommand.ChangeScenario(scenarioDropdown.selection.index)
+      case EditDone(`colsInput`) => onDimensionChange()
+      case EditDone(`rowsInput`) => onDimensionChange()
+    }
+
+    private def onDimensionChange(): Unit =
+      val maybeRows = rowsInput.text.toIntOption
+      val maybeCols = colsInput.text.toIntOption
+
+      (maybeRows, maybeCols) match
+        case (Some(rows), Some(cols)) if rows > 0 && cols > 0 =>
+          Simulation set UICommand.SetScenarioSize(rows, cols)
+          println(s"passing ($rows $cols)")
+        case _ => None
