56f3640722f19a8b16d4140ffa1bae8c9dfaecc8
MHS-20 <muhamad.salman2001@gmail.com>
2025-07-20 18:25:18 +0200

    chore: removed unsued execption type

--- src/main/scala/it/unibo/model/exceptions/Exceptions.scala
+++ src/main/scala/it/unibo/model/exceptions/Exceptions.scala
@@ -1,6 +1,5 @@
 package it.unibo.model.exceptions
 
 case object FailedPlannerBuildException extends Exception
-case object FailedPlannerExecutionException extends Exception
 case object AgentNotBuiltException extends Exception
 case class PlanNotFoundException(msg: String) extends Exception(msg)
