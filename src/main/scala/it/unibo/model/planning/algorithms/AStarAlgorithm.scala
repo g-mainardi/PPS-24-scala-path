93d70fb616a3f61daf5fdb3c8ac28b276e300a33
Gabriele Basigli <84378604+Basigli@users.noreply.github.com>
2025-07-22 18:05:42 +0200

    chore: removed old code

--- src/main/scala/it/unibo/model/planning/algorithms/AStarAlgorithm.scala
+++ src/main/scala/it/unibo/model/planning/algorithms/AStarAlgorithm.scala
@@ -31,7 +31,7 @@ object AStarAlgorithm extends PathFindingAlgorithm:
             originalTile match
               case Some(t) => delta = Position(t.x, t.y) - prev
               case _ => None
-            delta = Position(0, 0)
+            // delta = Position(0, 0)
           Direction.allDirections.find(_.vector == delta) match
             case Some(dir) => _reconstructPath(prev, dir :: acc)
             case _ => _reconstructPath(prev, acc)
